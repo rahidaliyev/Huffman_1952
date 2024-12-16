bf6a3bc9efa5ed12363dc1078773a4129bc8e3c7
rahidaliyev <rahidaliyev18@gmail.com>
2024-12-16 23:36:42 +0300

    predefined method added to py

--- arrayInput.py
+++ arrayInput.py
@@ -12,3 +12,15 @@ def InputArray(arr,num,sum):
                 break
 
     return(arr)
+
+
+def InputArrayPredefined(arr, num, sum):
+    predefined_values = [0.2, 0.18, 0.1, 0.1, 0.1, 0.06, 0.06, 0.04, 0.04, 0.04, 0.04, 0.03, 0.01]
+    for n in predefined_values:
+        if round(sum + n, 2) > 1:
+            print("Array sum cannot be more than 1. Please check the predefined values.")
+            return arr
+        else:
+            arr.append(n)
+            sum = round(sum + n, 2)
+    return arr
