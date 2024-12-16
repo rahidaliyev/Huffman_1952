d1f9c895445921bf92a5b3be85ab89351a4fd1f3
rahidaliyev <rahidaliyev18@gmail.com>
2024-12-16 23:35:46 +0300

    deleted redundant code

--- calculateToRightDirection.py
+++ calculateToRightDirection.py
@@ -1,7 +1,7 @@
 def CalculateRightDirection(arr=[]):
+    print(arr)
     while len(arr)>1:
         a = arr.pop() + arr.pop(-1)
-        arr = arr[:-2]
         arr.append(a)
         arr.sort(reverse=True)
         print(arr)
